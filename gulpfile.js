1c4ba153b977ce4c700b656fe2d14949e6812df4
Anthony Ho <ahomnky11@gmail.com>
2016-01-06 16:03:13 +0000

    submission

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var sassdoc = require('sassdoc');
+
+// ... variables
+var autoprefixerOptions = {
+  browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+};
+
+var sassdocOptions = {
+  dest: './public/sassdoc'
+};
+
+var input = './styles/**/*.scss';
+var output = './src/css';
+
+gulp.task('sass', function () {
+  return gulp
+    .src(input)
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest(output))
+});
+
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+
+gulp.task('sassdoc', function () {
+  return gulp
+    .src(input)
+    .pipe(sassdoc(sassdocOptions))
+    .resume();
+});
+
+gulp.task('sass', function () {
+  return gulp
+    .src(input)
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(gulp.dest(output));
+});
+
+gulp.task('watch', function() {
+  return gulp
+    // Watch the input folder for change,
+    // and run `sass` task when something happens
+    .watch(input, ['sass'])
+    // When there is a change,
+    // log a message in the console
+    .on('change', function(event) {
+      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+});
+
+gulp.task('default', ['sass', 'watch' /*, possible other tasks... */]);
+
---
+++ readme.md
@@ -0,0 +1,29 @@
+# Gulp.js Sass Starter
+
+
+
+This is a simple starter for commando Sass work. Great for poking around and learning how to configure gulp.js and easy to inject into projects. You'll need [node.js](http://nodejs.org/) and [gulp.js](http://gulpjs.com/) installed.
+
+
+## Instructions:
+ 
+	$ npm install
+
+	$ gulp watch
+
+	Edit /styles/main.scss -> save
+
+Gulp will create a main.min.css and a main.css file in the /styles dir.
+
+## Add to an existing project
+
+1.Copy package.json and gulpfile.js to your project's root.
+
+1.Edit the stylesDir variable on line #9 of gulpfile.js to tell gulp where your styles are.
+
+3.Open terminal and cd to your project's folder then run these commands
+
+	$ npm install
+	$ gulp watch
+
+Original author: Hugo Giraudel
